aedd9cab65bc468f5d14f1f54965f4d3447cdd4b
tomekbuszman <tomasz.buszman0309@gmail.com>
2017-12-17 21:47:57 +0100

    An Interactive Component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,9 +5,15 @@ import './index.css';
 
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+     }
     render() {
         return (
-            <button className="square">
+            <button className="square" onClick={() => this.setState({value: 'X'})>
                 {this.props.value}
             </button>
         );
